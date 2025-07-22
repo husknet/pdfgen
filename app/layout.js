d68586bd784ff9cabc8daf9a9dc278343811dc61
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-07-21 21:36:21 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,7 @@
-// app/layout.js
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{margin: 0, fontFamily: 'sans-serif'}}>{children}</body>
     </html>
   );
 }
